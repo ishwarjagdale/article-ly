27e4e76a23195139c04f389a9471808cefceb6a5
Ishwar Jagdale <ishwarrules@gmail.com>
2022-01-08 08:06:32 +0530

    changed app.js n added libs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Navigate, Route, Routes, useLocation} from 'react-router-dom';
+import Home from './views/Home';
+import Post from "./views/Post";
+import Dashboard from "./views/Dashboard";
+import SignUpPage from "./views/SignUpPage";
+import {Logout} from "./api/SignUp";
+import ProfilePage from "./views/ProfilePage";
+import Settings from "./views/Dashboard/Settings";
+import NewStory from "./views/NewStory";
+
+const AuthRoutes = ({children}: {children: JSX.Element}) => {
+    let location = useLocation();
+    return (
+      localStorage.hasOwnProperty("user") ?
+          children :
+          <Navigate to={"/sign-in"} state={{from: location}} replace/>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route path={"/"} element={<Home/>} />
+          <Route path={"/sign-in"} element={<SignUpPage/>}/>
+          <Route path={"/logout"} element={<AuthRoutes><Logout/></AuthRoutes>} />
+          <Route path={"/dashboard"} element={<AuthRoutes><Dashboard/></AuthRoutes>}/>
+          <Route path={"/settings"} element={<AuthRoutes><Settings/></AuthRoutes>}/>
+          <Route path={"/new-story"} element={<AuthRoutes><NewStory/></AuthRoutes>}/>
+          <Route path={"/s/:postURL"} element={<Post/>}/>
+          <Route path={"/@:username"} element={<ProfilePage/>}/>
+      </Routes>
+    </Router>
   );
 }
 
